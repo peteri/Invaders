c9f50ef115d3a592000072c3f4162e972669d8a9
Peter Ibbotson <peter@ibbotson.co.uk>
2020-11-07 13:58:34 +0000

    Initial Revision

---
+++ WpfInvaders/WpfInvaders/LineRender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace WpfInvaders
+{
+    public static class LineRender
+    {
+        private const int ScreenWidth = 32;
+        private const int ScreenHeight = 28;
+        public static byte[] Screen = new byte[ScreenWidth * ScreenHeight];
+        public static byte[] BitmapChar = new byte[32 * 8];
+
+        public static byte[] RenderLine(int line)
+        {
+            var returnData = new byte[ScreenWidth];
+            int startOfLine = (line / 8) * ScreenWidth;
+            int cellLine = line & 0x07;
+            for (int i = 0; i < ScreenWidth; i++)
+            {
+                byte c = Screen[startOfLine++];
+                int index = c * 8 + cellLine;
+                returnData[i] = (c < 32) ? BitmapChar[index] : SpriteData.Characters[index];
+            }
+            return returnData;
+        }
+    }
+}
---
+++ WpfInvaders/WpfInvaders/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfInvaders
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WriteableBitmap frame;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 256; i++)
+                LineRender.BitmapChar[i] = (byte)i;
+            for (int i = 0; i < 256; i++)
+                LineRender.Screen[i] = (byte)i;
+            frame = new WriteableBitmap(256, 224, 96, 96, PixelFormats.BlackWhite, null);
+            imgScreen.Source = frame;
+        }
+
+        private void btnFrameAdance_Click(object sender, RoutedEventArgs e)
+        {
+            for (int line = 0; line < 224; line++)
+            {
+                Int32Rect rect = new Int32Rect(0, 0, 256, 1);
+                frame.WritePixels(rect, LineRender.RenderLine(line), 32, 0, line);
+            }
+        }
+    }
+}
