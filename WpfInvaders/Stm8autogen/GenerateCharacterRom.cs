e9fcd91fe444be34ec541270526718b03c2c3f23
Peter Ibbotson <peter@ibbotson.co.uk>
2021-08-14 14:54:18 +0100

    Added code to character rom generator to pre-copy in players shields into the udg overlap space.

--- WpfInvaders/Stm8autogen/GenerateCharacterRom.cs
+++ WpfInvaders/Stm8autogen/GenerateCharacterRom.cs
@@ -11,6 +11,8 @@ namespace Stm8autogen
         {
             List<string> asmLines = new List<string>();
             List<string> incLines = new List<string>();
+            var player = new PlayerData();
+            player.Reset();
             asmLines.Add("stm8/");
             asmLines.Add(";=============================================");
             asmLines.Add("; Generated file from the WPF character map");
@@ -52,17 +54,34 @@ namespace Stm8autogen
                 asmLines.Add(";==============");
                 for (int i = 0; i < 256; i += 8)
                 {
-                    asmLines.Add(String.Format("\tDC.B ${0:X2},${1:X2},${2:X2},${3:X2},${4:X2},${5:X2},${6:X2},${7:X2}  ; ${8:X2} - ${9:X2}",
-                        CharacterRom.Characters[(i + 0) * 8 + j],
-                        CharacterRom.Characters[(i + 1) * 8 + j],
-                        CharacterRom.Characters[(i + 2) * 8 + j],
-                        CharacterRom.Characters[(i + 3) * 8 + j],
-                        CharacterRom.Characters[(i + 4) * 8 + j],
-                        CharacterRom.Characters[(i + 5) * 8 + j],
-                        CharacterRom.Characters[(i + 6) * 8 + j],
-                        CharacterRom.Characters[(i + 7) * 8 + j],
-                        i, i + 7
-                        ));
+                    if (i < 0x20)
+                    {
+                        asmLines.Add(String.Format("\tDC.B ${0:X2},${1:X2},${2:X2},${3:X2},${4:X2},${5:X2},${6:X2},${7:X2}  ; ${8:X2} - ${9:X2}",
+                            LineRender.BitmapChar[(i + 0) * 8 + j],
+                            LineRender.BitmapChar[(i + 1) * 8 + j],
+                            LineRender.BitmapChar[(i + 2) * 8 + j],
+                            LineRender.BitmapChar[(i + 3) * 8 + j],
+                            LineRender.BitmapChar[(i + 4) * 8 + j],
+                            LineRender.BitmapChar[(i + 5) * 8 + j],
+                            LineRender.BitmapChar[(i + 6) * 8 + j],
+                            LineRender.BitmapChar[(i + 7) * 8 + j],
+                            i, i + 7
+                            ));
+                    }
+                    else
+                    {
+                        asmLines.Add(String.Format("\tDC.B ${0:X2},${1:X2},${2:X2},${3:X2},${4:X2},${5:X2},${6:X2},${7:X2}  ; ${8:X2} - ${9:X2}",
+                            CharacterRom.Characters[(i + 0) * 8 + j],
+                            CharacterRom.Characters[(i + 1) * 8 + j],
+                            CharacterRom.Characters[(i + 2) * 8 + j],
+                            CharacterRom.Characters[(i + 3) * 8 + j],
+                            CharacterRom.Characters[(i + 4) * 8 + j],
+                            CharacterRom.Characters[(i + 5) * 8 + j],
+                            CharacterRom.Characters[(i + 6) * 8 + j],
+                            CharacterRom.Characters[(i + 7) * 8 + j],
+                            i, i + 7
+                            ));
+                    }
                 }
             }
             asmLines.Add("\tend");
